a56903144ae810245b048e0e8bb600f166213c98
Hendra Ndra <hendra@artamedianet.co.id>
2023-12-02 10:00:58 +0700

    add mysqli

--- Pertemuan2/dbConnection.php
+++ Pertemuan2/dbConnection.php
@@ -5,9 +5,9 @@
     $db_user = 'root';
     $db_pass = '';
 
-    $connect = mysql_connect($db_host, $db_user, $db_pass, $db_name);
+    $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-    if (mysql_connect_error()) {
+    if (mysqli_connect_error()) {
         echo "gagal melakukan koneksi ke db_host:$db_host " + mysql_connect_error();
     };
 ?>
