364703217a6ec0f2a8f32ba887b7348b8bd7c684
Hendra Ndra <hendra@artamedianet.co.id>
2023-12-02 09:59:09 +0700

    add connect db handler

--- Pertemuan2/dbConnection.php
+++ Pertemuan2/dbConnection.php
@@ -7,7 +7,7 @@
 
     $connect = mysql_connect($db_host, $db_user, $db_pass, $db_name);
 
-    (mysql_connect_error()) {
-        echo "gagal melakukan koneksi ke db_host:$db_host" : mysql_connect_error();
+    if (mysql_connect_error()) {
+        echo "gagal melakukan koneksi ke db_host:$db_host " + mysql_connect_error();
     };
 ?>
