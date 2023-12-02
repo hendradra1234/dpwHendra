bf5c6a94129b4869aa5f6e7af32646175b7debe1
Hendra Ndra <hendra@artamedianet.co.id>
2023-12-02 09:54:59 +0700

    add connect db handler

---
+++ Pertemuan2/dbConnection.php
@@ -0,0 +1,13 @@
+<?php
+    $db_host = 'localhost';
+    $db_name = 'semester1';
+    $db_port = '';
+    $db_user = 'root';
+    $db_pass = '';
+
+    $connect = mysql_connect($db_host, $db_user, $db_pass, $db_name);
+
+    (mysql_connect_error()) {
+        echo "gagal melakukan koneksi ke db_host:$db_host" : mysql_connect_error();
+    };
+?>
