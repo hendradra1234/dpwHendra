85759784f9ee0fc5f5825d35e0412b7e3ef088ef
Hendra Ndra <hendra@artamedianet.co.id>
2023-12-09 08:43:35 +0700

    add responses

--- Pertemuan3/save.php
+++ Pertemuan3/save.php
@@ -6,5 +6,11 @@ $tmptLahir = $_POST['tmp_lahir'];
 $tglLahir = $_POST['tgl_lahir'];
 $jenisKelamin = $_POST['jk'];
 
-$result = mysqli_query($connection, "INSERT INTO tbl_mahasiswa VALUES('$nim', '$name', '$tmptLahir', '$tglLahir', '$jenisKelamin');")
+$result = mysqli_query($connection, "INSERT INTO tbl_mahasiswa VALUES('$nim', '$name', '$tmptLahir', '$tglLahir', '$jenisKelamin');");
+
+if($result) {
+    echo "<h1>Error, Cannot save Data</h1>";
+} else {
+    echo "<h1>Data Saved Successfully</h1>";
+}
 ?>
