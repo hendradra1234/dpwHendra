e5275759eb1c31dbad9f2e5e93c3a97e94c265dc
Hendra Ndra <hendra@artamedianet.co.id>
2023-12-09 08:45:10 +0700

    flip logic

--- Pertemuan3/save.php
+++ Pertemuan3/save.php
@@ -9,8 +9,8 @@ $jenisKelamin = $_POST['jk'];
 $result = mysqli_query($connection, "INSERT INTO tbl_mahasiswa VALUES('$nim', '$name', '$tmptLahir', '$tglLahir', '$jenisKelamin');");
 
 if($result) {
-    echo "<h1>Error, Cannot save Data</h1>";
-} else {
     echo "<h1>Data Saved Successfully</h1>";
+} else {
+    echo "<h1>Error, Cannot save Data</h1>";
 }
 ?>
